165dd7c3c77ed252842a7e2481930a15dffbb191
Snaju003 <117941685+Snaju003@users.noreply.github.com>
2024-12-29 03:12:08 +0530

    socket url updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Replace with your backend server URL
+        target: 'https://chit-chat-q27g.onrender.com', // Replace with your backend server URL
         changeOrigin: true,
         secure: false
       }
